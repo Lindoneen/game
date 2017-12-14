5e2b6bc55760a1de75cdc65911fbe7391b1f4bf6
Lindoneen <Lindoneen>
2017-12-15 02:11:01 +0300

    Sozdan zagalovochnii fail v kotorom obyavlen class

---
+++ game/game/Player.h
@@ -0,0 +1,18 @@
+//Player.h
+#ifndef Player_h
+#define Player_h
+
+using namespace sf;
+class Player { // класс Игрока
+public:
+float x, y, w, h, dx, dy, speed; //координаты игрока х и у, высота и ширина спрайта 
+								//ускорение и скорость
+	int dir; //направление игрока
+	std::string File; //имя файла
+	Image image;//сфмл изображение
+	Texture texture;//сфмл текстура
+	Sprite sprite;//сфмл спрайт
+	Player(std::string F, float X, float Y, float W, float H, int DIR, float SPEED);
+	void update(float time);
+};
+#endif
